56d05e1a1fa6a7947f771d71a58e6ad3e95e4b0b
madhuracodes <madhura84@gmail.com>
2025-03-26 14:06:07 +0100

    added a footer and links to Github and netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project is created by Madhura, open-sourced on
+        <a
+          href="https://github.com/madhuracodes/react-weather-app"
+          target="_blank"
+        >
+          GitHub{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://maddy-reacts.netlify.app/" target="_blank">
+          {" "}
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
