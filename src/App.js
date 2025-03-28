e6de723030e742c838117702efad304c22903eec
madhuracodes <madhura84@gmail.com>
2025-03-28 10:16:43 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project is created by Madhura, open-sourced on
-        <a
-          href="https://github.com/madhuracodes/react-weather-app"
-          target="_blank"
-        >
-          GitHub{" "}
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://maddy-reacts.netlify.app/" target="_blank">
-          {" "}
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project is created by Madhura, open-sourced on
+          <a
+            href="https://github.com/madhuracodes/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            GitHub{" "}
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://maddy-reacts.netlify.app/" target="_blank">
+            {" "}
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
