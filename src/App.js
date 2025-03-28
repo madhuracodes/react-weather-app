d61473c9e51545680ea480caff4ec0d4e1a2621b
madhuracodes <madhura84@gmail.com>
2025-03-28 12:08:52 +0100

    corrected a warning about target blanck

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ export default function App() {
           <a
             href="https://github.com/madhuracodes/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             GitHub{" "}
           </a>{" "}
           and hosted on{" "}
-          <a href="https://maddy-reacts.netlify.app/" target="_blank">
+          <a
+            href="https://maddy-reacts.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             {" "}
             Netlify
           </a>
