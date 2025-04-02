53a0d262719ae2a82162ca3ea507be7a21c27fe7
madhuracodes <madhura84@gmail.com>
2025-04-02 09:21:02 +0200

    Added CSS to weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,11 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -29,7 +33,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="mostly cloudy"
           />{" "}
-          7ºC
+          <span className="temperature">7</span>
+          <span className="unit">ºC</span>
         </div>
         <div className="col-6">
           <ul>
