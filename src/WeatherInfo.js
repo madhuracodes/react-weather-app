c3d0fd470a51b30e8408c777c41adfa11399c216
madhuracodes <madhura84@gmail.com>
2025-04-13 11:05:36 +0200

    added feature so date shows only when searched for default city

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,25 +3,24 @@ import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
+  const isBerlin = props.data.city.toLowerCase() === "berlin";
+
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
+        <li>{isBerlin && <FormattedDate date={props.data.date} />}</li>
         <li>{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
-
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6 mt-3">
           <ul>
             <li>Humidity: {props.data.humidity}</li>
-            <li>Wind: {props.data.wind}km/hr </li>
+            <li>Wind: {props.data.wind}km/hr</li>
           </ul>
         </div>
       </div>
